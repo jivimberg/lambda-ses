2697e136ddf8596f8a11a70d7456876fc66ae02d
Juan Ignacio Vimberg <juan.vimberg@oracle.com>
2016-11-24 13:51:38 -0800

    Sending email with HTML content

--- src/main/java/com/budilov/lambda/ses/EmailService.java
+++ src/main/java/com/budilov/lambda/ses/EmailService.java
@@ -4,7 +4,16 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Region;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClient;
-import com.amazonaws.services.simpleemail.model.*;
+import com.amazonaws.services.simpleemail.model.Body;
+import com.amazonaws.services.simpleemail.model.Content;
+import com.amazonaws.services.simpleemail.model.Destination;
+import com.amazonaws.services.simpleemail.model.Message;
+import com.amazonaws.services.simpleemail.model.SendEmailRequest;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
 
 class EmailService {
 
@@ -17,19 +26,26 @@ class EmailService {
         Destination destination = new Destination().withToAddresses(TO);
 
         Content subject = new Content().withData(email.getSubject() + " (from " + email.getFrom() + ")");
-        Content textBody = new Content().withData(email.getMessage());
-        Body body = new Body().withText(textBody);
 
-        Message message = new Message().withSubject(subject).withBody(body);
+        try {
+            InputStream inputStream = PropUtil.class.getResourceAsStream("/form.html");
+            String formHtml = new BufferedReader(new InputStreamReader(inputStream))
+                    .lines()
+                    .collect(Collectors.joining("\n"));
 
-        SendEmailRequest request = new SendEmailRequest().withSource(FROM)
+            Content textBody = new Content().withData(formHtml);
+            Body body = new Body().withHtml(textBody);
+
+            Message message = new Message().withSubject(subject).withBody(body);
+
+            SendEmailRequest request = new SendEmailRequest().withSource(FROM)
                 .withDestination(destination)
                 .withMessage(message);
 
-        try {
+
             System.out.println("Attempting to send an email through Amazon SES by using the AWS SDK for Java...");
             AmazonSimpleEmailServiceClient client = new AmazonSimpleEmailServiceClient(AWS_CREDENTIALS);
-            Region REGION = Region.getRegion(Regions.EU_WEST_1);
+            Region REGION = Region.getRegion(Regions.US_EAST_1);
             client.setRegion(REGION);
 
             client.sendEmail(request);
